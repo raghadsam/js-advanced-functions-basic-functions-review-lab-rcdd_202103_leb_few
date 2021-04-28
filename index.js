7aa168a8f2953ea27dbe995f2a1b403381fc875d
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-28 09:47:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,18 +26,11 @@ let Calculator={
   }
 };
 function actionApplyer(integ,array){
-  return [MultiplyBy2(integ),Add1000()]
+  let result1,result2,result3;
+  return [
+        function(){return result1=`${integ}*2`; },
+        function(){return result2=`${result1}+1000;`},
+        function(){return result3=`${result2}%7`}
+  ];
+}
 
-}
-function MultiplyBy2(integ){
-  let result1=integ*2;
-  return result1;
-}
-function Add1000(result1){
-  let result2=result1+1000;
-  return result2;
-}
-function Modulo7(result2){
-  let result3=result2%7;
-  return result3;
-}
